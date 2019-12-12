2e84d24fd33c04a8942b464f230da7942cc4435f
Mona Lisa <asdf@dfdas.sa>
2019-12-12 12:25:48 +0600

    1

--- app/Boutique.php
+++ app/Boutique.php
@@ -22,7 +22,7 @@ class Boutique extends Model
 
     protected $appends = ['firstImage', 'categoriesName', 'averageRating'];
 
-    public $with = ['categories', 'tradingHouses', 'products'];
+    public $with = ['categories', 'tradingHouses', 'products', 'reviews', 'recommended', 'related'];
 
     public function categories()
     {
