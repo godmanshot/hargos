2cbfe4b3cf2a3fa2e22e2a095259499fa2bdd398
godmanshot <l4ch4wk@gmail.com>
2019-12-19 11:01:55 +0600

    boutique card correction2

--- resources/views/boutique.blade.php
+++ resources/views/boutique.blade.php
@@ -56,6 +56,11 @@
                                     <a target="_blank" class="mrc__plugin_like_button" href="https://connect.mail.ru/share" data-mrc-config="{'cm' : '2', 'sz' : '20', 'st' : '1', 'tp' : 'mm'}">Нравится</a>
                                     <script src="https://connect.mail.ru/js/loader.js" type="text/javascript" charset="UTF-8"></script>
                                 </li>
+                                <li>
+                                    <div id="fb-root"></div>
+                                    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/ru_RU/sdk.js#xfbml=1&version=v5.0"></script>
+                                    <div class="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button" data-size="small"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Поделиться</a></div>
+                                </li>
                                 <!-- <li>
                                     <script type='text/javascript' src='https://platform-api.sharethis.com/js/sharethis.js#property=5df850c338258c00124bd2ad&product=inline-share-buttons&cms=website' async='async'></script>
                                     <div class="sharethis-inline-share-buttons"></div>
