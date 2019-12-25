79469fab22352e079392b3480ab0b4e64e642a6a
godmanshot <l4ch4wk@gmail.com>
2019-12-25 10:50:44 +0600

    corrections2

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -125,7 +125,9 @@
                                 <div>
                                     <div class="geek-recommendation__block">
                                         <div>
-                                            <img src="{{Voyager::image($item->image)}}">
+                                            <a href="{{$item->boutiqueLink}}">
+                                                <img src="{{Voyager::image($item->image)}}">
+                                            </a>
                                             <div class="geek-recommendation__inner-block">
                                                 <h3>{{$item->getTranslatedAttribute('name')}}</h3>
                                                 <p>{{$item->boutiqueCategories}}</p>
@@ -385,7 +387,9 @@
                                 <div class="popular-products__block">
                                     <div>
                                         <div class="flexible">
-                                            <img src="{{Voyager::image($item->image)}}">
+                                            <a href="{{$item->boutiqueLink}}">
+                                                <img src="{{Voyager::image($item->image)}}">
+                                            </a>
                                             <div class="popular-products__inner-block">
                                                 <h3>{{$item->getTranslatedAttribute('name')}}</h3>
                                                 <p>
