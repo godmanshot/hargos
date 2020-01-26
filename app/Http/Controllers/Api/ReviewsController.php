d084f149950be0b5cca1c36905e921861979190b
Yuri <kim010698@gmail.com>
2020-01-26 11:45:32 +0600

    Review

--- app/Http/Controllers/Api/ReviewsController.php
+++ app/Http/Controllers/Api/ReviewsController.php
@@ -9,13 +9,13 @@ use App\Http\Controllers\Controller;
 class ReviewsController extends Controller
 {
     public function create(Request $request, Boutique $boutique) {
-    
         $boutique->reviews()->create([
             'name' => $request->name,
             'date' => now(),
             'review' => $request->review,
             'rating' => $request->rating,
         ]);
-    
+        
+        return response('Отзыв оставлен', 201);
     }
 }
