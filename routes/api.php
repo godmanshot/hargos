5e35de8928eaf7b69edc92389a94636571da2496
Yuri <kim010698@gmail.com>
2020-01-29 16:46:04 +0600

    Fix

--- routes/api.php
+++ routes/api.php
@@ -96,7 +96,7 @@ Route::middleware('cors')->namespace('Api')->group(function() {
 Route::middleware(['cors', 'auth:api'])->group(function() {
     Route::get('user', 'Api\UserController@index');
     Route::put('user/update', 'Api\UserController@update');
-    Route::get('/favorite', 'Api\FavoriteConwetroller@index');
+    Route::get('/favorite', 'Api\FavoriteController@index');
     Route::post('/favorite/{boutique}', 'Api\FavoriteController@add');
     Route::delete('/favorite/{boutique}', 'Api\FavoriteController@delete');
 
