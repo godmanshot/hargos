024170ebfa44e5e60548f490582018cb85da2f12
lachin <l4ch4wk@gmail.com>
2020-04-06 17:07:11 +0600

    qweqwe

--- resources/js/reviews.js
+++ resources/js/reviews.js
@@ -1,16 +1,20 @@
+import Swal from "sweetalert2";
+
 if ($("div").is(".reviews")) {
     for (i = 0; i < $('.avatar__wrapper').length; i++) {
-        if ($(".avatar__wrapper img")[i].currentSrc != "") {
-            $('.avatar__wrapper')[i].classList.remove("avatar__bg")
-        }
-        else {
-            $('.avatar__wrapper')[i].classList.add("avatar__bg")
-        }
+        $('.avatar__wrapper')[i].classList.add("avatar__bg")
     }
     $('.leave__review-wrapper .star-rating__input').on('click', function() {
         $(this).parent('label').nextAll('label').addClass('star-rating__checked');
         $(this).parent('label').prevAll('label').removeClass('star-rating__checked');
         $(this).nextAll('.leave__review-wrapper .star-rating__input').trigger('click');
         $(this).parent('label').addClass('star-rating__checked');
+    });
+    let sendReviewBtn = document.querySelector('.leave__review-btn');
+    sendReviewBtn.addEventListener('click', e => {
+        e.preventDefault();
+        Swal.fire({
+            title: 'Благодарим за обратную связь. Ваш отзыв успешно добавлен',
+        });
     });
 }
